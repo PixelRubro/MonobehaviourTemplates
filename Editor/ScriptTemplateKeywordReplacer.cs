c87e6c083a692f58f83f6b01c4b24c3abdf86dd2
msavioti <angelosavioti@gmail.com>
2022-10-05 20:15:21 -0300

    feat: created asset

---
+++ Editor/ScriptTemplateKeywordReplacer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using System.IO;
+
+namespace SoftBoiledGames.MonobehaviourTemplates
+{
+    /// <summary>
+    /// Author: Alexey Demonov
+    /// </summary>
+    public class ScriptTemplateKeywordReplacer : UnityEditor.AssetModificationProcessor
+    {
+        // If there would be more than one keyword to replace, add a Dictionary.
+        public static void OnWillCreateAsset(string metaFilePath)
+        {
+            string fileName = Path.GetFileNameWithoutExtension(metaFilePath);
+
+            if (!fileName.EndsWith(".cs"))
+            {
+                return;
+            }
+
+            string actualFilePath = $"{Path.GetDirectoryName(metaFilePath)}{Path.DirectorySeparatorChar}{fileName}";
+            string content = File.ReadAllText(actualFilePath);
+            string newcontent = content.Replace("#PROJECTNAME#", PlayerSettings.productName);
+
+            if (content != newcontent)
+            {
+                File.WriteAllText(actualFilePath, newcontent);
+                AssetDatabase.Refresh();
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# MonobehaviourTemplates
+# Monobehaviour Templates
+
 Templates to replace Unity's default template for new C# scripts.
+
+Under the following path:
+
+> *C:\Program Files\Unity\Hub\Editor\"Editor version folder"\Editor\Data\Resources\ScriptTemplates\\*  
+
+You will find a file named:  
+
+> *81-C# Script-NewBehaviourScript.cs.txt*
+
+Replace the content of this file with the content of any file under *Templates* and save.
+
+Now, the standard Monobehaviour script template is changed when creating new scripts.  
+
+When using the *NamespaceTemplate*, the script `ScriptTemplateKeywordReplacer.cs` will exchange `#PROJECTNAME#` for the project's name, simulating the act of moving the class to a namespace.
